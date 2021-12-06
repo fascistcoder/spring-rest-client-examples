10452be107fec27154a31db5b707656d0b9f610c
Pulkit Aggarwal <gargpulkit234@gmail.com>
2021-12-06 17:53:05 +0530

    Data Models

---
+++ src/main/java/com/example/spingrestclient/SpingRestClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.spingrestclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingRestClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpingRestClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spingrestclient/SpingRestClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spingrestclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingRestClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
