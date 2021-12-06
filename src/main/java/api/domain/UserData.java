e0519f90b6b79c077253eefbad220bf80b772996
Pulkit Aggarwal <gargpulkit234@gmail.com>
2021-12-06 18:24:29 +0530

    Refactor Data Model

---
+++ src/main/java/api/domain/UserData.java
@@ -0,0 +1,21 @@
+package api.domain;
+
+import java.util.List;
+
+/**
+ * @author <a href="pulkit.aggarwal@upgrad.com">Pulkit Aggarwal</a>
+ * @version 1.0
+ * @since 06/12/21
+ */
+public class UserData {
+
+    List<User> data;
+
+    public List<User> getData() {
+        return data;
+    }
+
+    public void setData(List<User> data) {
+        this.data = data;
+    }
+}
